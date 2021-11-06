4458fe1c5eaa037876d20997b473a6ba1188333a
João Henrique <joaohenriquetdj@gmail.com>
2021-11-06 14:43:13 -0300

    Add FK of department in seller

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -12,13 +12,14 @@ namespace SalesWebMvc.Models
         public double BaseSalary { get; set; }
         public DateTime BirthDate { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
         {
         }
 
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary,  Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
